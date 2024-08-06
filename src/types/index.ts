2953b004a5ff41ea76342d03aae58842368934c7
ErnestoPelayo <42719997+ErnestoPelayo@users.noreply.github.com>
2024-08-06 11:46:29 -0700

    zod added for validation in progress

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,20 @@
+import {z} from 'zod'
+
+export const projectSchema = z.object({
+  _id: z.string(),
+  projectName: z.string(),
+  clientName:z.string(),
+  description:z.string()
+})
+
+export type ProjectClass = z.infer<typeof projectSchema>
+export type ProjectFormData = Pick<ProjectClass,'clientName' | 'projectName' | 'description'>
+
+/*
+
 export type ProjectClass = {
     projectName:string,
       clientName:string,
       description:string,
 }
+      */
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,12 +1,13 @@
 import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
-import { ProjectClass } from "../../types"
+import { ProjectClass, ProjectFormData } from "../../types"
 import ErrorMessage from "../../components/ErrorMessage"
+import ProjectForm from "../../components/ProjectForm"
 
 
 const CreateProjectView = () => {
 
-  const initialValues: ProjectClass = {
+  const initialValues: ProjectFormData = {
     projectName: "",
     clientName: "",
     description: "",
@@ -14,14 +15,14 @@ const CreateProjectView = () => {
 
   const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
-  const handleForm = (data: ProjectClass) => {
+  const handleForm = (data: ProjectFormData) => {
     console.log(data)
   }
 
   return (
     <div className="mt-10">
       <h1 className="text-5xl font-black"> Crear Proyecto </h1>
-      <p className="text-2xl font-light text-gray-500 mt-5"> Lleba ek siguiente formulario para crear un proyecto </p>
+      <p className="text-2xl font-light text-gray-500 mt-5"> Llena el siguiente formulario para crear un proyecto </p>
       <nav className="py-4">
         <Link
           className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xs font-bold cursor-pointer transition-colors"
@@ -37,66 +38,11 @@ const CreateProjectView = () => {
           onSubmit={handleSubmit(handleForm)}
           noValidate
         >
-          <div className=" mb-5 space-y-3">
-            <label htmlFor="projectName" className="text-sm uppercase font-bold">
-              Nombre del Proyecto
-            </label>
-            <input
-              type="text"
-              id="projectName"
-              className=" w-full p-3 via-orange-50 border-gray-200"
-              {
-              ...register("projectName", {
-                required: "El titulo del proyecto es obligatorio"
-              })
-              }
-            />
-            {
-              errors.projectName && (
-                <ErrorMessage >{errors.projectName?.message} </ErrorMessage>
-              )
-            }
-          </div>
-          <div className=" mb-5 space-y-3">
-            <label htmlFor="projectName" className="text-sm uppercase font-bold">
-              Nombre del cliente 
-            </label>
-            <input
-              type="text"
-              id="clientName"
-              className=" w-full p-3 via-orange-50 border-gray-200"
-              {
-              ...register("clientName", {
-                required: "El nombre del cliente es obligatorio"
-              })
-              }
-            />
-            {
-              errors.clientName && (
-                <ErrorMessage >{errors.clientName?.message} </ErrorMessage>
-              )
-            }
-          </div>
-          <div className=" mb-5 space-y-3">
-            <label htmlFor="projectName" className="text-sm uppercase font-bold">
-              Descripción 
-            </label>
-            <input
-              type="text"
-              id="description"
-              className=" w-full p-3 via-orange-50 border-gray-200"
-              {
-              ...register("description", {
-                required: "La descripcion es obligatoria"
-              })
-              }
-            />
-            {
-              errors.description && (
-                <ErrorMessage >{errors.description?.message} </ErrorMessage>
-              )
-            }
-          </div>
+          <ProjectForm 
+            register={register}
+            errors ={errors}
+          />
+          
           <input
             type="submit" value="Crear Proyecto"
             className=" bg-fuchsia-600 hover:bg-fuchsia-700 w-full text-white uppercase font-bold cursor-pointer transition-colors"
